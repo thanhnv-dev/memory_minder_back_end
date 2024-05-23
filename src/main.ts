5972933a3b8bf0097cf12279f0f4c60058f8b105
thanhnv <thanhnv2@newwave.vn>
2024-05-23 14:20:03 +0700

    Hot fix

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
 
-export default bootstrap;
+export default async () => {
+  await bootstrap();
+};
